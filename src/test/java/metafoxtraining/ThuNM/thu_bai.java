78df95f81f7d0994e8c007a0fa340d2ba0b6708d
thunm <thunm@younetgroup.com>
2023-02-08 16:16:56 +0700

    add name thu_bai.java

---
+++ src/test/java/metafoxtraining/ThuNM/thu_bai.java
@@ -0,0 +1,7 @@
+package test.java.metafoxtraining.ThuNM;
+import java.util.*;
+public class thu_bai {
+    public static void main(String[] args) {
+        System.out.print("Nguyen Minh Thu");
+    }
+}
