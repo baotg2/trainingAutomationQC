d90bb8180ac2309aba649a16536245938a8dba87
Vincent <103025986+baotg2@users.noreply.github.com>
2023-02-09 14:59:19 +0700

    add maven dependency

--- src/test/java/metafoxtraining/TrucNQ/test.java
+++ src/test/java/metafoxtraining/TrucNQ/test.java
@@ -1,8 +1,13 @@
 package metafoxtraining.TrucNQ;
 
-
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class test {
-    WebDriver driver;
+   public static void main(String[] args)
+   {
+       WebDriver driver= new ChromeDriver();
+       driver.get("http://testng.org/doc/maven.html");
+       driver.manage().window().maximize();
+   }
 }
